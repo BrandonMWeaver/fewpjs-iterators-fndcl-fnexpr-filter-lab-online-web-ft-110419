b226c398c1e29fd0c190a1135f31355f1c39d5d4
Brandon Weaver <a7xncob@gmail.com>
2020-02-25 21:40:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function findMatching(drivers, string) {
   return drivers.filter(function(driver) {
-    return driver === string;
+    return driver.toLowerCase() === string.toLowerCase();
   });
 }
