b2e39a1ca6f51bfc54b7478ca3e921c8244aed6a
Brandon Weaver <a7xncob@gmail.com>
2020-02-25 21:45:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function fuzzyMatch(drivers, string) {
 }
 
 function matchName(drivers, string) {
-  
+  return drivers.filter(function(driver) {
+    return driver.name.toLowerCase() === string.toLowerCase();
+  });
 }
